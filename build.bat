39ab9b490f836bb87145470ade5a4103444bbdb9
https://github.com/fpawel <binf@mail.ru>
2019-02-15 21:38:07 +0300

    minor changes

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 SET APP_DIR=%HOMEDRIVE%%HOMEPATH%\.elco
 SET GOARCH=386
-buildmingw32 go build -o %APP_DIR%\elco.exe github.com/fpawel/elco/cmd/app
+buildmingw32 go build -o %APP_DIR%\elco.exe github.com/fpawel/elco/cmd/elco
 start %APP_DIR%
