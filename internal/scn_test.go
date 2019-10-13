2621d585d2a77cd6d4cce074f646055943537c63
binf1611@gmail.com <fpawel>
2019-10-13 19:13:05 +0300

    scn: line by line

--- internal/scn_test.go
+++ internal/scn_test.go
@@ -8,11 +8,6 @@ import (
 	"testing"
 )
 
-type Instruction struct {
-	Name  string
-	Param int
-}
-
 func TestScript(t *testing.T) {
 
 	//var script []func()
@@ -21,55 +16,56 @@ func TestScript(t *testing.T) {
 	pInt := parsec.Token(`[0-9]+`, "INT")
 	pFloat := parsec.Token(`[+-]?([0-9]+\.[0-9]*|\.[0-9]+|[0-9]+)`, "FLOAT")
 
+	wh := func(f func(parsec.Queryable)) parsec.ASTNodify {
+		return func(name string, s parsec.Scanner, node parsec.Queryable) parsec.Queryable {
+			f(node)
+			return node
+		}
+	}
+
 	pInstruction := ast.OrdChoice(
 		"INSTRUCTION", nil,
 		ast.And("TEMPERATURE",
-			func(name string, s parsec.Scanner, node parsec.Queryable) parsec.Queryable {
-				return node
-			},
+			wh(func(node parsec.Queryable) {
+
+			}),
 			parsec.Atom("temperature", "TEMPERATURE-KEYWORD"),
 			pFloat,
 		),
 		ast.And("GAS",
-			func(name string, s parsec.Scanner, node parsec.Queryable) parsec.Queryable {
-				return node
-			},
+			wh(func(node parsec.Queryable) {
+
+			}),
 			parsec.Atom("gas", "GAS-KEYWORD"),
 			pInt,
 		),
 		ast.And("PAUSE",
-			func(name string, s parsec.Scanner, node parsec.Queryable) parsec.Queryable {
-				return node
-			},
+			wh(func(node parsec.Queryable) {
+
+			}),
 			parsec.Atom("pause", "PAUSE-KEYWORD"),
 			pInt,
 		),
 		ast.And("SAVE",
-			func(name string, s parsec.Scanner, node parsec.Queryable) parsec.Queryable {
-				return node
-			},
+			wh(func(node parsec.Queryable) {
+
+			}),
 			parsec.Atom("save", "SAVE-KEYWORD"),
 		),
 	)
 	pLine := ast.And("INSTRUCTION-LINE", nil, pInstruction, ast.End("END"))
 
-	s := `
-save
-pause -1222
+	s := `save
+pause 1222
 temperature 20
 gas 178
-gas 1  bubu
-pause 12
-`
+gas 1   
+pause 12`
 
 	lineScanner := bufio.NewScanner(strings.NewReader(s))
 	lineNo := 1
 	for lineScanner.Scan() {
 		str := strings.TrimSpace(lineScanner.Text())
-		if len(str) == 0 {
-			lineNo++
-			continue
-		}
 		ast.Reset()
 		scanner := parsec.NewScanner([]byte(str))
 		node, _ := ast.Parsewith(pLine, scanner)
@@ -92,17 +88,4 @@ pause 12
 		//fmt.Println(str , ":")
 		//ast.Prettyprint()
 	}
-}
-
-func setTemperature(t float64) {
-	fmt.Println("set temperature", t)
-}
-func setGas(gas int) {
-	fmt.Println("set gas", gas)
-}
-func pause(n int) {
-	fmt.Println("pause", n)
-}
-func save() {
-	fmt.Println("save")
 }
